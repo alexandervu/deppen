b588ca3b39c5e8d0b83395fb848cdfcf3cc90d0f
Alexander Vu <alexander.vu.tuyet@gmail.com>
2021-02-18 18:55:36 +0100

    extended 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World - extendet';
+  ctx.body = 'Hello World - extendet 3';
 });
 
 app.listen(process.env.PORT || 3000);
